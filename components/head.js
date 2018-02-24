325c317f60c4f788d4a884fa4b7e396574a51e1c
Terry Tilley <terry@terrytilley.com>
2018-02-24 21:13:32 +0000

    head.js default props

--- components/head.js
+++ components/head.js
@@ -2,33 +2,23 @@ import React from 'react';
 import NextHead from 'next/head';
 import { string } from 'prop-types';
 
-const defaultOGURL = '';
-const defaultOGImage = '';
-const defaultDescription = '';
-
 const Head = props => (
   <NextHead>
     <meta charSet="UTF-8" />
-    <title>{props.title || ''}</title>
-    <meta
-      name="description"
-      content={props.description || defaultDescription}
-    />
+    <title>{props.title}</title>
+    <meta name="description" content={props.description} />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="icon" sizes="192x192" href="/static/touch-icon.png" />
     <link rel="apple-touch-icon" href="/static/touch-icon.png" />
     <link rel="mask-icon" href="/static/favicon-mask.svg" color="#49B882" />
     <link rel="icon" href="/static/favicon.ico" />
-    <meta property="og:url" content={props.url || defaultOGURL} />
-    <meta property="og:title" content={props.title || ''} />
-    <meta
-      property="og:description"
-      content={props.description || defaultDescription}
-    />
-    <meta name="twitter:site" content={props.url || defaultOGURL} />
+    <meta property="og:url" content={props.url} />
+    <meta property="og:title" content={props.title} />
+    <meta property="og:description" content={props.description} />
+    <meta name="twitter:site" content={props.url} />
     <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:image" content={props.ogImage || defaultOGImage} />
-    <meta property="og:image" content={props.ogImage || defaultOGImage} />
+    <meta name="twitter:image" content={props.ogImage} />
+    <meta property="og:image" content={props.ogImage} />
     <meta property="og:image:width" content="1200" />
     <meta property="og:image:height" content="630" />
   </NextHead>
@@ -43,9 +33,9 @@ Head.propTypes = {
 
 Head.defaultProps = {
   title: '',
-  url: defaultOGURL,
-  ogImage: defaultOGImage,
-  description: defaultDescription,
+  url: '',
+  ogImage: '',
+  description: '',
 };
 
 export default Head;
