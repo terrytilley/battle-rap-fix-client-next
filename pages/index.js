369c321dfdd416dbf942231c485cc20f58d36ef5
Terry Tilley <terry@terrytilley.com>
2018-02-25 19:15:35 +0000

    add apollo graphql

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,42 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+import withData from '../lib/withData';
+
 import Head from '../components/head';
 import Navbar from '../components/Navbar';
 
-const Home = () => (
+const Home = ({ data: { allUsers, allLeagues } }) => (
   <div>
     <Head title="Home" />
     <Navbar />
 
-    <h1>Home Page</h1>
+    <h1>Usernames</h1>
+    <ul>{allUsers.map(u => <li key={u.id}>{u.username}</li>)}</ul>
+
+    <h1>Leagues</h1>
+    <ul>{allLeagues.map(l => <li key={l.id}>{l.name}</li>)}</ul>
   </div>
 );
 
-export default Home;
+Home.propTypes = {
+  data: PropTypes.objectOf(PropTypes.any).isRequired,
+};
+
+const query = gql`
+  {
+    allUsers {
+      id
+      username
+    }
+    allLeagues {
+      id
+      name
+    }
+  }
+`;
+
+const GraphQL = graphql(query)(Home);
+
+export default withData(GraphQL);
